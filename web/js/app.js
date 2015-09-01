23679c82ce6293431cc9c39bb016fc5b4edeaab4
George Chailazopoulos <george.chailazopoulos@gmail.com>
2015-09-01 13:42:10 +0300

    Add partials and ng route configuration

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,22 @@
-var app = angular.module('ebay', ['ngResource']);
+'use strict';
+
+var app = angular.module('ebay', ['ngResource','ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when(
+        '/welcome',
+        {
+            templateUrl: 'partials/welcome.html',
+            controller: 'MyCtrl1'
+        });
+    $routeProvider.when(
+        '/store',
+        {
+            templateUrl: 'partials/store.html',
+            controller: 'EbayController'
+        });
+    $routeProvider.otherwise(
+        {
+            redirectTo: '/store'
+        });
+});
