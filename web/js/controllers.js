31ef606504480f7fae6cbc5320ea218880031d62
George Chailazopoulos <george.chailazopoulos@gmail.com>
2015-09-14 16:36:03 +0300

    Fix issues with map in itemview

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -9,14 +9,14 @@ function containsObject(obj, list) {
     return false;
 }
 
-var removeByAttr = function(arr, attr, value){
+var removeByAttr = function (arr, attr, value) {
     var i = arr.length;
-    while(i--){
-        if( arr[i]
+    while (i--) {
+        if (arr[i]
             && arr[i].hasOwnProperty(attr)
-            && (arguments.length > 2 && arr[i][attr] === value ) ){
+            && (arguments.length > 2 && arr[i][attr] === value )) {
 
-            arr.splice(i,1);
+            arr.splice(i, 1);
 
         }
     }
@@ -25,7 +25,7 @@ var removeByAttr = function(arr, attr, value){
 
 
 angular.module('ebay')
-    .controller('GlobalController', function($scope, $state, Category, Item, Authentication) {
+    .controller('GlobalController', function ($scope, $state, Category, Item, Authentication) {
         $scope.triedToBid = false;
 
         //fetch all categories. Issues a GET to /api/categories
@@ -34,22 +34,22 @@ angular.module('ebay')
         //fetch all items. Issues a GET to /api/items
         $scope.items = Item.query();
 
-        $scope.bid = function(item) {
-           var userRole = Authentication.getRole();
-            if  (userRole == "GUEST") {
+        $scope.bid = function (item) {
+            var userRole = Authentication.getRole();
+            if (userRole == "GUEST") {
                 $scope.triedToBid = true;
                 $("#signInModal").modal();
-            } else  {
-                $state.go("viewItem",{id:item.id});
+            } else {
+                $state.go("viewItem", {id: item.id});
             }
         }
 
-        $scope.goToAccount = function() {
+        $scope.goToAccount = function () {
             if (Authentication.getUserName()) {
                 $state.go("viewUser", {username: Authentication.getUserName()});
             }
         }
-        $scope.goToAdmin = function() {
+        $scope.goToAdmin = function () {
             if (Authentication.getRole() == "ADMIN") {
                 $state.go("listUsers");
             }
@@ -58,75 +58,78 @@ angular.module('ebay')
         $scope.getRole = Authentication.getRole;
     })
 
-    .controller('CategoryListController', function($scope, Category) {
+    .controller('CategoryListController', function ($scope, Category) {
         //fetch all categories. Issues a GET to /api/categories
         $scope.categories = Category.query();
     })
 
-    .controller('ItemListController', function($scope, Item) {
+    .controller('ItemListController', function ($scope, Item) {
         //fetch all items. Issues a GET to /api/items
         $scope.items = Item.query();
     })
-    .controller('ItemViewController', function($scope, $state, $stateParams, Item, Bid, Authentication) {
+    .controller('ItemViewController', function ($scope, $state, $stateParams, Item, Bid, Authentication) {
         $scope.imageIndex = 0;
         $scope.imageCount = 0;
 
         $scope.role = Authentication.getRole;
         $scope.bid = new Bid();
 
-        $scope.initMaps = function(){
+        var map = new google.maps.Map(
+            document.getElementById("map-container"), {
+                zoom: 14
+            });
+        var marker = null;
+
+        $scope.initMaps = function () {
             var location = new google.maps.LatLng($scope.item.location.longitude, $scope.item.location.latitude);
 
-            var map = new google.maps.Map(
-                document.getElementById("map-container"), {
-                    center: location,
-                    zoom: 14
-                });
+            if (marker != null) marker.setMap(null);
 
-            var marker = new google.maps.Marker({
+            marker = new google.maps.Marker({
                 position: location,
                 map: map,
-                title:$scope.item.location.name
+                title: $scope.item.location.name
             });
 
             marker.setMap(map);
+            map.panTo(location);
         }
 
         $scope.item = Item.get({
             id: $stateParams.id
-        }, function(){
+        }, function () {
             $scope.imageCount = $scope.item.images.length;
             $scope.bid.amount = parseFloat($scope.item.currently) + 1;
             $scope.initMaps();
         });
 
-        $scope.createBid = function(){
+        $scope.createBid = function () {
             $scope.bid.$save({
                 username: Authentication.getUserName(),
                 itemId: $scope.item.id
-            }, function(){
+            }, function () {
                 $state.go("viewUser", {username: Authentication.getUserName()})
             });
         }
     })
 
-    .controller('SellerItemsViewController', function($scope, $stateParams, SellerItem) {
+    .controller('SellerItemsViewController', function ($scope, $stateParams, SellerItem) {
         $scope.items = SellerItem.query();
     })
 
-    .controller('SellerItemCreateController', function($scope, $stateParams, Seller,
-                                                       Authentication, $state , SellerItem, Upload, $timeout, Category) {
+    .controller('SellerItemCreateController', function ($scope, $stateParams, Seller,
+                                                        Authentication, $state, SellerItem, Upload, $timeout, Category) {
 
         $scope.categories = Category.query();
-        $scope.addCategory = function(cat){
-            if (typeof $scope.item.categories == 'undefined'){
+        $scope.addCategory = function (cat) {
+            if (typeof $scope.item.categories == 'undefined') {
                 $scope.item.categories = []
             }
-            if (!containsObject({name: cat}, $scope.item.categories)){
+            if (!containsObject({name: cat}, $scope.item.categories)) {
                 $scope.item.categories.push({name: cat});
             }
         }
-        $scope.removeCategory = function(cat){
+        $scope.removeCategory = function (cat) {
             console.log("called");
             removeByAttr($scope.item.categories, "name", cat.name);
         }
@@ -135,7 +138,7 @@ angular.module('ebay')
         $scope.fileList = [];
         $scope.log = '';
         $scope.item = new SellerItem();
-        $scope.createItem = function() {
+        $scope.createItem = function () {
             $scope.item.images = [];
             for (var i = 0; i < $scope.fileList.length; i++) {
                 $scope.item.images.push({
@@ -172,9 +175,9 @@ angular.module('ebay')
                         }).progress(function (evt) {
                             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                             $scope.log = 'progress: ' + progressPercentage + '% ' +
-                                evt.config.file.name + '\n' + $scope.log;
+                            evt.config.file.name + '\n' + $scope.log;
                         }).success(function (data, status, headers, config) {
-                            $timeout(function() {
+                            $timeout(function () {
                                 $scope.log = 'file: ' + config.file.name + ', Response: ' + JSON.stringify(data) + '\n' + $scope.log;
                             });
                         });
@@ -184,11 +187,11 @@ angular.module('ebay')
         };
 
     })
-    .controller('SellerItemEditController', function($scope, $stateParams, SellerItem,
-                                            Authentication, $state, $stateParams, Upload, $timeout) {
+    .controller('SellerItemEditController', function ($scope, $stateParams, SellerItem,
+                                                      Authentication, $state, $stateParams, Upload, $timeout) {
         $scope.item = SellerItem.get({
             id: $stateParams.id
-        }, function(){
+        }, function () {
             for (var i = 0; i < $scope.item.images.length; i++) {
                 $scope.fileList.push({
                     name: $scope.item.images[i].url,
@@ -201,7 +204,7 @@ angular.module('ebay')
         $scope.fileList = [];
         $scope.log = '';
 
-        $scope.editItem = function() {
+        $scope.editItem = function () {
             for (var i = 0; i < $scope.fileList.length; i++) {
                 $scope.item.images.push({
                     caption: $scope.fileList[i].caption,
@@ -238,9 +241,9 @@ angular.module('ebay')
                         }).progress(function (evt) {
                             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                             $scope.log = 'progress: ' + progressPercentage + '% ' +
-                                evt.config.file.name + '\n' + $scope.log;
+                            evt.config.file.name + '\n' + $scope.log;
                         }).success(function (data, status, headers, config) {
-                            $timeout(function() {
+                            $timeout(function () {
                                 $scope.log = 'file: ' + config.file.name + ', Response: ' + JSON.stringify(data) + '\n' + $scope.log;
                             });
                         });
@@ -252,125 +255,124 @@ angular.module('ebay')
     })
 
 
-
-    .controller('UserViewController', function($scope, $state, $stateParams, $http, User, ReceivedMessage) {
-       /* $scope.newauction = {
-            currently : null,
-            numberOfBids : null,
-            started : new Date(),
-            seller : null,
-            bids : null
-        };*/
+    .controller('UserViewController', function ($scope, $state, $stateParams, $http, User, ReceivedMessage) {
+        /* $scope.newauction = {
+         currently : null,
+         numberOfBids : null,
+         started : new Date(),
+         seller : null,
+         bids : null
+         };*/
 
         $scope.user = User.get({
             username: $stateParams.username
         });
 
         $scope.unreadMessages = 0;
-        var messages = ReceivedMessage.query({},function(){
-            for (var i =0; i < messages.length; i++) {
+        var messages = ReceivedMessage.query({}, function () {
+            for (var i = 0; i < messages.length; i++) {
                 if (!messages[i].toSeen) $scope.unreadMessages++;
             }
         });
 
         /*$scope.createAuction = function() {
 
-            var data = $scope.newauction;
-            data.location = {
-                name : $scope.newauction.country,
-                lat: $scope.newauction.location.split(",")[0],
-                lng: $scope.newauction.location.split(",")[1]};
+         var data = $scope.newauction;
+         data.location = {
+         name : $scope.newauction.country,
+         lat: $scope.newauction.location.split(",")[0],
+         lng: $scope.newauction.location.split(",")[1]};
 
-            $http.post("/api/items",data).then(function(res){
-                console.log(res);
+         $http.post("/api/items",data).then(function(res){
+         console.log(res);
 
-            });
+         });
 
-        };
+         };
 
-        $state.go("viewUser.createItem");*/
+         $state.go("viewUser.createItem");*/
 
     })
-    .controller('AdminViewController', function($scope, $http) {
+    .controller('AdminViewController', function ($scope, $http) {
 
-        $http.get("/api/users").then(function(res) {
+        $http.get("/api/users").then(function (res) {
             $scope.users = res.data;
-        }, function(response) {
+        }, function (response) {
             //something went wrong
         });
 
 
     })
 
-    .controller('AdminController', function($scope, User) {
-        $scope.reject = function() {
+    .controller('AdminController', function ($scope, User) {
+        $scope.reject = function () {
             $scope.user.accountStatus = 'REJECTED';
             $scope.user.$update();
         };
 
-        $scope.accept = function() {
+        $scope.accept = function () {
             $scope.user.accountStatus = 'ACCEPTED';
             $scope.user.$update();
         };
 
-        $scope.revoke = function() {
+        $scope.revoke = function () {
             $scope.user.accountStatus = 'REVOKED';
             $scope.user.$update();
         };
     })
 
-    .controller('UserCreateController', function($scope, $state, $stateParams, User) {
+    .controller('UserCreateController', function ($scope, $state, $stateParams, User) {
         $scope.user = new User();  //create new user instance. Properties will be set via ng-model on UI
 
         //create a new user. Issues a POST to /api/users
-        $scope.addUser = function() {
-            $scope.user.$save(function() {
+        $scope.addUser = function () {
+            $scope.user.$save(function () {
                 //$state.go('welcome'); // on success go back to home
             });
         };
     })
 
-    .controller('UserSignInController', function($scope, $state, $stateParams, Authentication) {
+    .controller('UserSignInController', function ($scope, $state, $stateParams, Authentication) {
         $scope.role = Authentication.getRole;
         $scope.signInStatus = "";
-        $scope.signIn = function() {
+        $scope.signIn = function () {
             $scope.signInStatus = "pending..";
-            Authentication.signIn($scope.credentials, function(success) {
+            Authentication.signIn($scope.credentials, function (success) {
                 if (success) {
                     $("#signInModal").modal('hide');
                     if (Authentication.getRole() == "ADMIN") {
                         $state.go("listUsers");
                     } else {
-                        $state.go("viewUser",{username:Authentication.getUserName()});
+                        $state.go("viewUser", {username: Authentication.getUserName()});
                     }
                 } else {
                     $scope.signInStatus = "Something went wrong";
                 }
             });
         }
-        $scope.signOut = function() {
+        $scope.signOut = function () {
             Authentication.signOut();
         }
     })
 
     //MESSAGE CONTROLLERS================
-    .controller('UserCreateMessageController', function($scope, SentMessage, Authentication, $state){
+    .controller('UserCreateMessageController', function ($scope, SentMessage, Authentication, $state) {
         $scope.message = new SentMessage()
 
-        $scope.createMessage = function(){
+        $scope.createMessage = function () {
             $scope.message.fromUsername = Authentication.getUserName()
             $scope.message.$save()
 
             $state.go("viewUser.sentMessages")
         }
     })
-    .controller('UserReceivedMessagesController', function($scope, ReceivedMessage){
-        $scope.messages = ReceivedMessage.query({},function(){
-            for (var i =0; i < $scope.messages.length; i++){
-                $scope.$watch('messages['+i+']', function(newMessage, oldMessage){
-                    if ( newMessage.toSeen && !oldMessage.toSeen) {
+    .controller('UserReceivedMessagesController', function ($scope, ReceivedMessage) {
+        $scope.messages = ReceivedMessage.query({}, function () {
+            for (var i = 0; i < $scope.messages.length; i++) {
+                $scope.$watch('messages[' + i + ']', function (newMessage, oldMessage) {
+                    if (newMessage.toSeen && !oldMessage.toSeen) {
                         $scope.$parent.unreadMessages--;
-                    } else if ( !newMessage.toSeen && oldMessage.toSeen) {
+                    } else if (!newMessage.toSeen && oldMessage.toSeen) {
                         $scope.$parent.unreadMessages++;
                     }
 
@@ -380,10 +382,10 @@ angular.module('ebay')
             }
         });
     })
-    .controller('UserSentMessagesController', function($scope, SentMessage){
-        $scope.messages = SentMessage.query({},function(){
-            for (var i =0; i < $scope.messages.length; i++){
-                $scope.$watch('messages['+i+']', function(newMessage, oldMessage){
+    .controller('UserSentMessagesController', function ($scope, SentMessage) {
+        $scope.messages = SentMessage.query({}, function () {
+            for (var i = 0; i < $scope.messages.length; i++) {
+                $scope.$watch('messages[' + i + ']', function (newMessage, oldMessage) {
                     console.log("updateMsg");
                     newMessage.$update()
                 }, true);
